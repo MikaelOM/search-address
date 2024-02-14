ea901a8a25cf8c1b53b04670cc7f22ef2fb1c2d0
Mikael Henrique <el.mh506@gmail.com>
2024-02-14 13:25:29 -0300

    styles adjusts

--- src/components/ShowDataLocality.tsx
+++ src/components/ShowDataLocality.tsx
@@ -9,20 +9,23 @@ interface ShowDataLocalityProps {
 }
 
 export function ShowDataLocality({ dataAddress, handleToGoBack }: ShowDataLocalityProps) {
+  console.log(!dataAddress)
   return (
     <div className="data-address-container">
-      <div className="data-address-header">
-        <button type="button" onClick={handleToGoBack}>
-          <HiArrowSmLeft size={28}/>
-        </button>
-        <h1>Address Infromation</h1>
+      <div className="data-address-content">
+        <div className="data-address-header">
+          <button type="button" onClick={handleToGoBack}>
+            <HiArrowSmLeft size={28}/>
+          </button>
+          <h1>Address Infromation</h1>
+        </div>
+        <section>
+          <p> <span>Postal Code:</span> { dataAddress.cep } </p>
+          <p> { dataAddress.logradouro } </p>
+          <p> { dataAddress.bairro } </p>
+          <p> { dataAddress.localidade } - { dataAddress.uf } </p>
+        </section>
       </div>
-      <section>
-        <p> <span>Postal Code:</span> { dataAddress.cep } </p>
-        <p> { dataAddress.logradouro } </p>
-        <p> { dataAddress.bairro } </p>
-        <p> { dataAddress.localidade } - { dataAddress.uf } </p>
-      </section>
     </div>
   )
 }
